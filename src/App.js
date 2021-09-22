ce43c9f3c9c961b272ba8a1abcea1e3019eec4ab
L1ndseyHerman <lherman21@jcu.edu>
2021-09-22 18:58:30 -0400

    Caught up to vid271: dynamic routes and params.

--- src/App.js
+++ src/App.js
@@ -3,10 +3,14 @@ import {Route} from 'react-router-dom';
 
 import Welcome from './pages/Welcome';
 import Products from './pages/Products';
+import ProductDetail from './pages/ProductDetail';
 import MainHeader from './components/MainHeader';
 
 //  "path" is a pre-coded prop that takes the path after the main website,
 //  so this could be like "www.mywebsite.com/welcome".
+
+// :productId the ":" is required, "productId" is a variable.
+//  www.mywebsite.com/product-detail/<any value>
 function App() {
   return (
     <div>
@@ -17,6 +21,9 @@ function App() {
         </Route>
         <Route path="/products">
           <Products />
+        </Route>
+        <Route path="/product-detail/:productId">
+          <ProductDetail />
         </Route>
       </main>
     </div>
